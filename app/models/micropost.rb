18f4fd57e32c9823de065a55e935b5b322301819
uangianlap <huangjian@protonmail.com>
2018-02-27 17:04:54 +0800

    add validates to user and micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
 	validates :content, length: { maximum: 140}
+	validates :content, presence: true
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+	validates :name, :email, presence: true
 	has_many :microposts
+
 end
